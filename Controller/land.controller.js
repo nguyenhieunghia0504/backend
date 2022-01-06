8859ba17f929a4948521f33cd59cab7eefeb6558
nvchien2811 <nvchien2811@gmail.com>
2022-01-06 20:15:50 +0700

    add-api-schedule

--- Controller/land.controller.js
+++ Controller/land.controller.js
@@ -1,4 +1,5 @@
 var db = require('../db')
+const Email = require('./sendEmail.controller');
 
 module.exports.getFullLand = (req,res)=>{
     const sql = "SELECT * FROM land";
@@ -19,6 +20,25 @@ module.exports.getLandDetails = (req,res)=>{
             return res.json({msg:err})
         }else{
             return res.json(result)
+        }
+    })
+}
+
+module.exports.addSchedule = (req,res)=>{
+    const {idCustommer,idLand,Time,Email} = req.body;
+    const sql = 'INSERT INTO schedule (`idCustomer`,`idLand`,`Time`) VALUES(?,?,?)'
+    db.query(sql,[idCustommer,idLand,Time],(err,result)=>{
+        if(err){
+            return res.json({msg:err})
+        }else{
+            //Send for customer
+            Email.SendEmail(Email,"Đặt lịch thành công",
+            `Bạn đã đặt lịch hẹn thành công vào ${new Date(Time).toLocaleString()}`
+            )
+            Email.SendEmail(process.env.EMAIL,"Nhận được lịch hẹn tư vấn",
+            `Khách đặt lịch tư vấn vào lúc ${new Date(Time).toLocaleString()}`
+            )
+            return res.json({msg:"Success"})
         }
     })
 }
--- Controller/test.controller.js
+++ Controller/test.controller.js
@@ -1,13 +1,16 @@
 const db = require('../db')
+const Email = require('./sendEmail.controller');
 
 module.exports.test = (req,res)=>{
-    const sql = "SELECT * FROM test"
-    db.query(sql,(err,result)=>{
-        if(err){
-            return res.json({msg:err})
-        }else{
-            console.log(result)
-            return res.json(result)
-        }
-    })
+    Email.SendEmail("nhnghia.18it3@vku.udn.vn","Xin chào","Đây là email tự động")
+    // const sql = "SELECT * FROM test"
+    // db.query(sql,(err,result)=>{
+    //     if(err){
+    //         return res.json({msg:err})
+    //     }else{
+    //         console.log(result)
+    //         Email.SendEmail("nhnghia.18it3@vku.udn.vn","Xin chào","Đây là email tự động")
+    //         return res.json(result)
+    //     }
+    // })
 }
